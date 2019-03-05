5e7050a79362dc280f5215151597217c7a56dcad
JohnMutina <johnmutina@gmail.com>
2019-03-05 22:38:10 +0100

    Add share flag functionality

--- Challenge - Flag Viewer/DetailViewController.swift
+++ Challenge - Flag Viewer/DetailViewController.swift
@@ -16,12 +16,24 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // if a flag has been selected
         if let flagToLoad = selectedFlag {
+            // set the image equal to the selected flag
             flagImage.image = UIImage(named: flagToLoad)
         }
+        
+        // add a button to the nav controller which calls the shareTapped method
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
     }
-    
-    
+    // create the method to share items
+    @objc func shareTapped() {
+        // create an activity on the images
+        let vc = UIActivityViewController(activityItems: [flagImage.image!], applicationActivities: [])
+        // link the action to the button to the right of the nav controller
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        // present the activity
+        present(vc, animated: true)
+    }
 
 }
